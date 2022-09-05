2df388f21d82c0d66b0da7c50fc34020d354f5a3
Gil <frogvsdogvsmarsmelo@gmail.com>
2022-09-05 10:09:43 +0300

    Added the src file for items

---
+++ src/items/items.controller.ts
@@ -0,0 +1,27 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { CreateItemDto } from './dto/create-item.dto';
+import { UpdateItemDto } from './dto/update-item.dto';
+
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
+  @Get()
+  findAll() {
+    return this.itemsService.getItems();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.itemsService.getItem(+id);
+  }
+}

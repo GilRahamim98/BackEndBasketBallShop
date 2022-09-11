5424d04f01466d4fb30b6f007a02c816f2c05846
Gil <frogvsdogvsmarsmelo@gmail.com>
2022-09-11 14:33:49 +0300

    Added update amount in units in stock value

--- dist/order_details/order_details.module.js
+++ dist/order_details/order_details.module.js
@@ -19,6 +19,7 @@ OrderDetailsModule = __decorate([
         imports: [typeorm_1.TypeOrmModule.forFeature([order_detail_entity_1.Order_Details])],
         controllers: [order_details_controller_1.OrderDetailsController],
         providers: [order_details_service_1.OrderDetailsService],
+        exports: [order_details_service_1.OrderDetailsService],
     })
 ], OrderDetailsModule);
 exports.OrderDetailsModule = OrderDetailsModule;
